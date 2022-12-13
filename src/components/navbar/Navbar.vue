e188d6d24d7b61cf31becb614b28521a2dd161b8
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-13 23:23:25 +0700

    fix

---
+++ src/components/navbar/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="px-4 bg-blue-50 flex w-full max-h-full">
+    <div class="w-1/5 flex-none mx-4 my-4 ">
+        <p class="font-medium text-xl">HRM-HT</p>
+    </div>
+    <div class="w-4/5 flex grow mx-4 my-4 ">
+        <div class="ml-auto flex gap-4">
+            <p>Login</p>
+            <p>Logout</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
