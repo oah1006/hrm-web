300c61d8d398881be0704c00fc36c8ffb05212bb
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-16 11:34:10 +0700

    setup VueCookies

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueCookies from 'vue-cookies'
 
 import './style.css'
 import './assets/main.css'
 
 const app = createApp(App)
 
+app.use(VueCookies, {
+
+})
+
 app.use(router)
 
 app.mount('#app')
+
