daeaa781784d9a879c7b2e8bbc6c15a7d6bf2fa2
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-16 11:39:31 +0700

    fetching data reset password

---
+++ src/components/auth/ResetPassword.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="w-full h-screen min-h-full bg-blue-100">
+        <div class="flex items-center justify-center h-screen">
+            <form @submit.prevent="handleSubmit" class="w-2/5 px-20 py-8 bg-white rounded-lg">
+                <p class="text-center font-medium text-3xl">Reset Password</p>
+                <p class="text-red-500 mt-1" v-if="error">{{ error.message }}</p>
+                <p class="mt-3">New password</p>
+                <input type="password" v-model="new_password" class="rounded-md px-2 py-2 mt-1 w-full border border-zinc-200" placeholder="Password" />
+                <p class="mt3">Confirmation Password</p>
+                <input type="password" v-model="new_password_confirmation" class="rounded-md px-2 py-2 mt-1 w-full border border-zinc-200" placeholder="Confirmation password" />             
+                <button class="px-4 py-2 bg-cyan-500 w-full mt-4 rounded-sm text-white">Tạo mật khẩu mới</button>
+            </form>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            new_password: '',
+            new_password_confirmation: '',
+            error: ''
+        }
+    },
+    methods: {
+        handleSubmit() {
+            const data = {
+                new_password: this.new_password,
+                new_password_confirmation: this.new_password_confirmation,
+                email: this.$route.query.email
+            }
+
+            const token = this.$cookies.get('tokenOtp')
+            
+            console.log(token)
+
+            axios
+            .post('http://127.0.0.1:8000/api/admin/auth/forgot-password', data, {
+                headers: {
+                    token: token
+                }
+            })
+            .then((response) => {
+                this.$router.push('/login')
+            })
+            .catch((error) => {
+                this.error = error.response.data
+            })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
