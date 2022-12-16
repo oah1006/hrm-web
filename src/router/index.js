ded354941666c4f484e7bb7e2b9dc5e537b6d09a
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-16 11:34:21 +0700

    create router page

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,39 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Dashboard from '../components/admin/Dashboard.vue'
+import Login from '../components/auth/Login.vue'
+import ForgotPassword from '../components/auth/ForgotPassword.vue'
+import ResetPassword from '../components/auth/ResetPassword.vue'
+import VerifyOTP from '../components/auth/VerifyOTP.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+    // admin
     {
       path: '/admin/dashboard',
       name: 'dashboard',
       component: Dashboard
     },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/forgot-password',
+      name: 'forgot-password',
+      component: ForgotPassword
+    },
+    {
+      path: '/reset-password',
+      name: 'reset-password',
+      component: ResetPassword
+    },
+    {
+      path: '/verify-otp',
+      name: 'verify-otp',
+      component: VerifyOTP
+    }
   ]
 })
 
