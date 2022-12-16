0eef15170762f2ebfc18af44bc5f4f8a81dd8d60
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-16 11:39:20 +0700

    fetching data login

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,15 +1,18 @@
 <template>
-    <div class="w-full h-full bg-blue-100">
-        <div class="flex items-center justify-center h-[700px]">
+    <div class="w-full h-screen min-h-full bg-blue-100">
+        <div class="flex items-center justify-center h-screen">
             <form @submit.prevent="handleSubmit" class="w-2/5 px-20 py-8 bg-white rounded-lg">
                 <p class="text-center font-medium text-3xl">Login</p>
                 <p class="text-center text-base font-thin mt-1">Welcome to HRM Hiệp thành!</p>
-                <p class="mt-2">Email</p>
+                <p class="text-red-500 mt-3" v-if="error">{{ error.message }}</p>
+                <p class="mt-1">Email</p>
                 <input type="text" name="email" v-model="email" class="rounded-md px-2 py-2 mt-1 w-full border border-zinc-200" placeholder="Username" />
-                <p class="text-red-500" v-if="error">{{ error.message }}</p>
                 <p class="mt-3">Password</p>
                 <input type="password" v-model="password" class="rounded-md px-2 py-2 mt-1 w-full border border-zinc-200" placeholder="Password" />
-                <button v-on:click="handleSubmit" class="px-4 py-2 bg-cyan-500 w-full mt-6 rounded-sm text-white">Đăng nhập</button>
+                <button class="px-4 py-2 bg-cyan-500 w-full mt-6 rounded-sm text-white">Đăng nhập</button>
+                <div class="flex">
+                    <a href="/forgot-password" class="ml-auto text-cyan-500 underline mt-1">Forgot password?</a>
+                </div>
             </form>
         </div>
     </div>
@@ -27,16 +30,24 @@
         },
         methods: {
             async handleSubmit() {
-                const response = await axios.post('http://127.0.0.1:8000/api/admin/auth/login', {
+                const data = {
                     email: this.email,
                     password: this.password
-                }).then(() => {
-                    this.$router.push('/admin/dashboard')
-                }).catch((error) => {
-                    this.error = error.response.data
-                });
+                }
 
-                
+                const instance = axios.create({
+                    withCredentials: true
+                })
+
+                instance
+                    .post('http://127.0.0.1:8000/api/admin/auth/login', data)
+                    .then((response) => {
+                        this.$cookies.set('token', response.data.token, 60 * 60 * 24)
+                        this.$router.push('/admin/dashboard')
+                    })
+                    .catch((error) => {
+                        this.error = error.response.data
+                    })
             }
         }
     }
