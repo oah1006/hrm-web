e804f55d2dc2f77da1ae3ba28bc4a9fa8a142033
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-16 11:39:52 +0700

    fetching data logout

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -5,15 +5,35 @@
     </div>
     <div class="w-4/5 flex grow mx-4 my-4 ">
         <div class="ml-auto flex gap-4">
-            <p>Login</p>
-            <p>Logout</p>
+            <button type="logout" @click="logout">Logout</button>
         </div>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
+  
 
+  methods: {
+    logout() {
+      const token = this.$cookies.get('token')
+
+      axios
+        .post('http://127.0.0.1:8000/api/admin/logout', {} ,{
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + token
+          }
+        }, {
+          withCredentials: true
+        })
+        .then((response) => {
+          this.$cookies.remove('token');
+          this.$router.push('/login')
+        })
+    }
+  }
 }
 </script>
