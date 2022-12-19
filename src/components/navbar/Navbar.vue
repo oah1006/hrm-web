b5dc9b39ab31116821a035606e5796cb43a18d6a
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-19 08:59:06 +0700

    create navbar component and fetching data with axios to logout function

--- src/components/navbar/Navbar.vue
+++ src/components/navbar/Navbar.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="px-4 bg-blue-50 flex w-full max-h-full">
-    <div class="w-1/5 flex-none mx-4 my-4 ">
+  <div class="flex items-center border-b shadow-md">
+    <div class="flex w-1/6 border-r py-3 px-10">
         <p class="font-medium text-xl">HRM-HT</p>
     </div>
-    <div class="w-4/5 flex grow mx-4 my-4 ">
-        <div class="ml-auto flex gap-4">
-            <button type="logout" @click="logout">Logout</button>
-        </div>
+    <div class="flex gap-8 items-center ml-auto mr-10">
+          <p class="text-red-500 font-medium">{{ user.first_name }}</p>
+          <a href="#" @click="logout">Logout</a>
     </div>
   </div>
 </template>
@@ -14,8 +13,9 @@
 <script>
 import axios from 'axios'
 export default {
-  
-
+  props: {
+    user: Object
+  },
   methods: {
     logout() {
       const token = this.$cookies.get('token')
