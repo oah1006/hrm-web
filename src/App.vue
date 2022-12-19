4557e1cf68128c3aee155dba3425e013ce606ac6
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-19 08:56:10 +0700

    clean file app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <script>
-  import Login from './components/auth/Login.vue'
-  import Navbar from './components/navbar/Navbar.vue'
-  import Dashboard from './components/admin/Dashboard.vue'
-
   export default {
-    components: {
-      Login,
-      Navbar, 
-      Dashboard
-    }
+
   }
 </script>
 
