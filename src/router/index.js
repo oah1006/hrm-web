bc7190d060ad4256b899fb04cf6a776fd485b48f
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-19 08:59:47 +0700

    create router all page now

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,10 @@ import Login from '../components/auth/Login.vue'
 import ForgotPassword from '../components/auth/ForgotPassword.vue'
 import ResetPassword from '../components/auth/ResetPassword.vue'
 import VerifyOTP from '../components/auth/VerifyOTP.vue'
+import IndexEmployee from '../components/admin/employee/IndexEmployee.vue'
+import CreateEmployee from '../components/admin/employee/CreateEmployee.vue'
+import UpdateEmployee from '../components/admin/employee/UpdateEmployee.vue'
+import DetailEmployee from '../components/admin/employee/DetailEmployee.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -13,6 +17,27 @@ const router = createRouter({
       path: '/admin/dashboard',
       name: 'dashboard',
       component: Dashboard
+    },
+    // employee
+    {
+      path: '/admin/employee',
+      name: 'employee',
+      component: IndexEmployee
+    },
+    {
+      path: '/admin/employee/create-employee',
+      name: 'createEmployee',
+      component: CreateEmployee
+    },
+    {
+      path: '/admin/employee/update-employee/:id',
+      name: 'updateEmployee',
+      component: UpdateEmployee
+    },
+    {
+      path: '/admin/employee/detail-employee/:id',
+      name: 'detailEmployee',
+      component: DetailEmployee
     },
     {
       path: '/login',
