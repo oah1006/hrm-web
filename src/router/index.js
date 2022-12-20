e32c4f3378ab1fcca9b9100518e426a564b1a9e5
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-20 11:34:29 +0700

    protected router when login

--- src/router/index.js
+++ src/router/index.js
@@ -4,12 +4,20 @@ import Login from '../components/auth/Login.vue'
 import ForgotPassword from '../components/auth/ForgotPassword.vue'
 import ResetPassword from '../components/auth/ResetPassword.vue'
 import VerifyOTP from '../components/auth/VerifyOTP.vue'
+
 import IndexEmployee from '../components/admin/employee/IndexEmployee.vue'
 import StoreEmployee from '../components/admin/employee/StoreEmployee.vue'
 import UpdateEmployee from '../components/admin/employee/UpdateEmployee.vue'
 import DetailEmployee from '../components/admin/employee/DetailEmployee.vue'
+
 import IndexDepartment from '../components/admin/department/IndexDepartment.vue'
 import StoreDepartment from '../components/admin/department/StoreDepartment.vue'
+import UpdateDepartment from '../components/admin/department/UpdateDepartment.vue'
+import DetailDepartment from '../components/admin/department/DetailDepartment.vue'
+
+import IndexLeaveType from '../components/admin/leave-type/IndexLeaveType.vue'
+import StoreLeaveType from '../components/admin/leave-type/StoreLeaveType.vue'
+import UpdateLeaveType from '../components/admin/leave-type/UpdateLeaveType.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -18,28 +26,43 @@ const router = createRouter({
     {
       path: '/admin/dashboard',
       name: 'dashboard',
-      component: Dashboard
+      component: Dashboard,
+      meta: {
+        requiresAuth: true
+      }
     },
     // employee
     {
       path: '/admin/employee',
       name: 'employee',
-      component: IndexEmployee
+      component: IndexEmployee,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/admin/employee/store-employee',
       name: 'storeEmployee',
-      component: StoreEmployee
+      component: StoreEmployee,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/admin/employee/update-employee/:id',
       name: 'updateEmployee',
-      component: UpdateEmployee
+      component: UpdateEmployee,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/admin/employee/detail-employee/:id',
       name: 'detailEmployee',
-      component: DetailEmployee
+      component: DetailEmployee,
+      meta: {
+        requiresAuth: true
+      }
     },
     // end employee
 
@@ -47,17 +70,70 @@ const router = createRouter({
     {
       path: '/admin/department',
       name: 'department',
-      component: IndexDepartment
+      component: IndexDepartment,
+      meta: {
+        requiresAuth: true
+      }
     },
     {
       path: '/admin/department/store-department',
       name: 'storeDepartment',
-      component: StoreDepartment
+      component: StoreDepartment,
+      meta: {
+        requiresAuth: true
+      }
     },
+    {
+      path: '/admin/department/update-department/:id',
+      name: 'updateDepartment',
+      component: UpdateDepartment,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin/department/detail-department/:id',
+      name: 'detailDepartment',
+      component: DetailDepartment,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    // end department
+
+    // leave-type
+    {
+      path: '/admin/leave-type',
+      name: 'leaveType',
+      component: IndexLeaveType,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin/leave-type/store-leave-type',
+      name: 'storeLeaveType',
+      component: StoreLeaveType,
+      meta: {
+        requiresAuth: true
+      }
+    },
+    {
+      path: '/admin/leave-type/update-leave-type/:id',
+      name: 'updateLeaveType',
+      component: UpdateLeaveType,
+      meta: {
+        requiresAuth: true
+      }
+    },
+
     {
       path: '/login',
       name: 'login',
-      component: Login
+      component: Login,
+      meta: {
+        guest: true
+      }
     },
     {
       path: '/forgot-password',
@@ -76,5 +152,31 @@ const router = createRouter({
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  const token = $cookies.get('token')
+  if (to.meta.requiresAuth) {
+    console.log(token)
+    if (token) {
+      next();
+    } else {
+      next({
+        name: 'login'
+      })
+    }
+  } else if (to.meta.guest) {
+    if (token) {
+      next({
+        name: 'dashboard'
+      })
+    } else {
+      next();
+    }
+  } else {
+    next()
+  }
+});
+
+
 
 export default router
