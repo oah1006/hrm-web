f485d31c8869030c0cbceebd5e9c121a72fa496a
OaH <81689054+oah1006@users.noreply.github.com>
2022-12-20 11:32:15 +0700

    create router updateLeaveType and create remove leave type in list

--- src/components/admin/leave-type/IndexLeaveType.vue
+++ src/components/admin/leave-type/IndexLeaveType.vue
@@ -56,8 +56,14 @@
                                 </svg>
                             </a>
 
-                            <a
-
+                            <router-link
+                                v-if="leaveType.id"
+                                :to="{
+                                    name: 'updateLeaveType',
+                                    params: {
+                                        id: leaveType.id
+                                    }
+                                }"
                             >
                                 <svg
                                 xmlns="http://www.w3.org/2000/svg"
@@ -73,8 +79,8 @@
                                     d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
                                 />
                                 </svg>
-                            </a>
-                            <button>
+                            </router-link>
+                            <button @click="removeLeaveType(leaveType.id)">
                                 <svg
                                 xmlns="http://www.w3.org/2000/svg"
                                 fill="none"
@@ -126,6 +132,19 @@ export default {
             })
             .then((response) => {
                 this.leaveTypes = response.data.data 
+            })
+        },
+        removeLeaveType(id) {
+            const token = this.$cookies.get('token')
+
+            axios
+            .delete('http://127.0.0.1:8000/api/admin/leave-types/' + id, {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+            .then((response) => {
+                location.reload();
             })
         }
     },
